5952cab177e2640ca4aeaac04c479d230dc66675
Quickmotions <fergusbhaak@gmail.com>
2023-09-05 23:06:10 +0100

    added functionality to filesystem.py

--- directory.py
+++ directory.py
@@ -1,26 +1,39 @@
 # directory.py -- Fergus Haak -- 05/09/2023
 from datetime import datetime
-
+from file import File
 
 class Directory:
 
-    def __init__(self, name):
-        self.name = name
-        self.files = []
-        self.subdr = []
-        self.date = datetime.now()
+    def __init__(self, name, parent: None):
+        self.name: str = name
+        self.files: list[File] = []
+        self.subdir: list[Directory] = []
+        self.date: datetime = datetime.now()
+        self.parent_dir: Directory = parent
 
-    def cr_subdr(self):
+    def cr_subdir(self, name):
+        self.subdir.append(Directory(name))
+
+    def dl_subdir(self):
         ...
 
-    def dl_subdr(self):
-        ...
-
-    def cr_file(self, file):
-        ...
+    def cr_file(self, name, content):
+        new_file = File(name)
+        if content: new_file.write(content)
+        self.files.append(new_file)
 
     def dl_file(self, file):
         ...
 
     def ls(self):
         ...
+
+    def dir(self):
+        for file in self.files:
+            yield file.name
+
+    def get_subdir(self, name):
+        for subdir in self.subdir:
+            if subdir.name == name:
+                return subdir
+
--- filesystem.py
+++ filesystem.py
@@ -1,44 +1,48 @@
 # filesystem.py -- Fergus Haak -- 05/09/2023
 from datetime import datetime
-from directory import Directory
-from file import File
+from pprint import pprint
 
+from directory import Directory
 
 class FileSystem:
     def __init__(self):
-        self.working_directory = Directory()
-        self.root_directory = self.working_directory
+        self.wrk_dir = Directory()
+        self.root = self.wrk_dir
         self.date = datetime.now()
 
-    def cr_dir(self):
+    def cr_dir(self, name):
         """creates a directory in working directory"""
-        ...
+        self.wrk_dir.cr_subdir(name)
 
-    def del_dir(self):
+    def del_dir(self, name):
         """deletes a directory in working directory"""
-        ...
+        self.wrk_dir.dl_subdir(name)
 
-    def cr_file(self):
+    def cr_file(self, name, content: str = None):
         """creates a file"""
-        ...
+        self.wrk_dir.cr_file(name, content)
 
-    def del_file(self):
+    def del_file(self, name):
         """deletes a file"""
-        ...
+        self.wrk_dir.dl_file(name)
 
-    def dir(self):
+    def dir(self) -> list:
         """list working directory"""
-        ...
+        return list(self.wrk_dir.dir())
 
-    def ch_dir(self):
+    def ch_dir(self, name):
         """change working directory"""
-        ...
+        self.wrk_dir = self.wrk_dir.get_subdir(name)
 
-    def save_fs(self):
+    def mv_bk(self):
+        """move back to previous directory"""
+        self.wrk_dir = self.wrk_dir.parent_dir
+
+    def sv_fs(self):
         """saves the file system to disc"""
         ...
 
-    def load_fs(self):
+    def ld_fs(self):
         """loads the file system from disc"""
         ...
 
